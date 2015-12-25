9e33c5cfda593999cf4ccd019b77e89ab5166a9b
tomzhao <329587820@qq.com>
2015-12-25 08:00:10 +0800

    web注解配置

---
+++ src/main/groovy/com/tom/demo/MybatisDemo.groovy
@@ -0,0 +1,19 @@
+package com.tom.demo
+
+import org.apache.ibatis.io.Resources
+import org.apache.ibatis.session.SqlSessionFactory
+import org.apache.ibatis.session.SqlSessionFactoryBuilder
+
+/**
+ * Created by tom on 15/12/24.
+ */
+class MybatisDemo {
+    public static void main(String[] args){
+        def source="/mybatis-config.xml"
+        InputStream inputStream = Resources.getResourceAsStream(source)
+        SqlSessionFactory sessionFactory = SqlSessionFactoryBuilder.build(inputStream)
+
+    }
+
+
+}
