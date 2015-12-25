3b039672069b2d1cf63ecaa27cf65425045ec036
tomzhao <329587820@qq.com>
2015-12-25 14:03:22 +0800

    基础代码

---
+++ src/main/java/com/tomweb/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.tomweb.config;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Anthor: Tom Zhao
+ * Date: 2015/12/25 0025
+ * Time: 13:54
+ */
+
+public class SecurityConfig {
+   /* @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }*/
+}
