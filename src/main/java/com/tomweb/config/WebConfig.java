f0182ec4f9dc2d49f447e05ce5e3560b608aa771
zhaoshichuan <zhaoshichuan@bxjr.com>
2016-02-24 19:20:18 +0800

    基础代码

--- src/main/java/com/tomweb/config/WebConfig.java
+++ src/main/java/com/tomweb/config/WebConfig.java
@@ -55,10 +55,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/js/");
-        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/");
-        registry.addResourceHandler("/img/**").addResourceLocations("/WEB-INF/img/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/");
         super.addResourceHandlers(registry);
     }
+
+
+
 }
